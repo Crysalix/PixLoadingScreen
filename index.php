02afaca23e137ec54d04d7ab98d271c390b159aa
Yannick G <yannick013@gmail.com>
2016-03-16 00:38:25 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+//A SteamAPI Key is needed to get users profile info (To get your key, go to http://steamcommunity.com/dev/apikey)
+$steamAPIKey = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; 
+
+//SteamID for Demo page
+$demoUrl = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'?steamid=76561198023541849'; //There's mine
+
+if (!isset($_GET['steamid'])) {
+    header("Location: $demoUrl");
+    exit();
+}else
+    $steamID = $_GET['steamid'];
+
+$url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='.$SteamAPIKey.'&steamids='.$steamID;
+$json = file_get_contents($url);
+$table = json_decode($json, true);
+$userData = $table['response']['players'][0];
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
+        <link rel="stylesheet" href="css/style.css" />
+        <title>My Loading Screen</title>
+    </head>
+    <body>
+        <header>
+        </header>
+        <section>
+            <div id="profile">
+                <?php echo '<img src="'.$userData['avatarfull'].'" />';?>
+            </div>
+        </section>
+        <footer>
+        </footer>
+    </body>
+</html>
